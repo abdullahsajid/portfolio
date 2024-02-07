882a0bb43ba9b20a9e2920f7bf4b5d0e63231964
Abdullah sajid <abdullahsajid521@gmail.com>
2024-02-07 15:31:06 +0500

    -modified ga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Home from './pages/Home';
-
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-Y38T7CPZ67');
+ReactGA.pageview(window.location.pathname);
 function App() {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "./fonts/TESLA.ttf"
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-Y38T7CPZ67');
-ReactGA.pageview(window.location.pathname + window.location.search);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
