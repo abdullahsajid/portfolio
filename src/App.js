e1cb6214392d46e44a85e72445f691ffc246a3e9
Abdullah sajid <abdullahsajid521@gmail.com>
2024-02-07 15:43:48 +0500

    -update pg react-ga4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Home from './pages/Home';
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-Y38T7CPZ67');
-ReactGA.pageview(window.location.pathname);
+import Home from "./pages/Home";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-Y38T7CPZ67");
+
 function App() {
   return (
     <>
-      <Home/>
+      <Home />
     </>
   );
 }
