b0e5a6581b6602fac76ff086d9acd44b9717a856
Abdullah sajid <abdullahsajid521@gmail.com>
2025-08-10 15:38:32 +0500

    chore: update year typo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex flex-col items-center justify-center mb-3'>
-        <div className='text-xs text-[#8e8e8e]'>Copyright 2024 © Abdullah Sajid</div>
+        <div className='text-xs text-[#8e8e8e]'>Copyright {new Date().getFullYear()} © Abdullah Sajid</div>
         <div className='text-xs text-[#8e8e8e] text-[10px]'>abdullahsajid521@gmail.com</div>
     </div>
   )
