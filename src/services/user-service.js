8414bd17437380c0e064a55601cc2b7c7562ed8e
Abdullah sajid <abdullahsajid521@gmail.com>
2025-08-26 00:43:59 +0500

    update

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,17 +1,17 @@
-export const chatApi = (payload,token) => {
+import axios from "axios";
+export const chatApi = (payload, token) => {
     return fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/chat`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
       },
-      body: payload,
+      body: JSON.stringify(payload),
     });
 };
 
 
-export const generateToken = () => {
-    return fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/auth/getToken`, {
-      method: "GET",
-    });
+export const generateToken = async () => {
+  const res = await axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}/auth/getToken`);
+  return res.data.token;
 };
